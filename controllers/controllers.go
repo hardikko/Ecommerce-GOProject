19a2ef81a3b82f05b5497b81ab4dcdcdcc2e8293
Hardik <kothari.hardik23@gmail.com>
2024-03-31 01:37:41 +0530

    created func in controllers

---
+++ controllers/controllers.go
@@ -0,0 +1,29 @@
+package controllers
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func HashPassword(password string) string {
+
+}
+
+func VerifyPass(userPass string, givenPass string) (bool, string) {
+
+}
+
+func Signup() gin.HandlerFunc {
+
+}
+
+func Login() gin.HandlerFunc {
+
+}
+
+func ProductViewerAdmin() gin.HandlerFunc {
+
+}
+
+func SearchProductByQuery() gin.HandlerFunc {
+
+}
