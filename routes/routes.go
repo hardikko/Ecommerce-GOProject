b4e9f7c85f34a9cc05245e3b351f372e20ae409a
Hardik <kothari.hardik23@gmail.com>
2024-03-30 21:58:00 +0530

    Created Port and Routes

--- routes/routes.go
+++ routes/routes.go
@@ -1,1 +1,15 @@
 package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/hardikko/ecommerce/controllers"
+)
+
+func UserRoutes(inRoutes *gin.Engine) {
+	inRoutes.POST("/users/signup", controllers.Signup())
+	inRoutes.POST("/users/login", controllers.Login())
+	inRoutes.POST("/admin/addproduct", controllers.ProductViewerAdmin())
+	inRoutes.GET("/users/productview", controllers.SearchProduct())
+	inRoutes.GET("/users/search", controllers.SearchProductByQuery())
+
+}
