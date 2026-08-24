40d0f2db262c0cd2274a9d8ace79c07a7c872b5e
Hardik <kothari.hardik23@gmail.com>
2026-08-24 16:18:20 +0530

    Updated Token File

--- tokens/tokengen.go
+++ tokens/tokengen.go
@@ -6,7 +6,7 @@ import (
 	"os"
 	"time"
 
-	"github.com/akhil/ecommerce-yt/database"
+	"github.com/hardikko/ecommerce/database"
 
 	jwt "github.com/dgrijalva/jwt-go"
 	"go.mongodb.org/mongo-driver/bson"
